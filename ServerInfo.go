ba380db4737d5c41c7f73de15262a241a7bbe4a5
Makrorof <makrorof@gmail.com>
2022-05-10 14:32:41 +0300

    ServerInfo degistirildi.

--- ServerInfo.go
+++ ServerInfo.go
@@ -3,8 +3,8 @@ package AmazonSessionAPIClient
 type ServerInfo struct {
 	ProxyCount                        int            `json:"proxyCount"`
 	SessionCreatorProxyCount          map[string]int `json:"sessionCreatorProxyCount"`
-	SessionCount                      int            `json:"sessionCount"`
-	UsableSessionCount                int            `json:"usableSessionCount"`
+	SessionCount                      map[string]int `json:"sessionCount"`
+	UsableSessionCount                map[string]int `json:"usableSessionCount"`
 	SessionsAreCreating               bool           `json:"sessionsAreCreating"`
 	SearchingForSessionCreatorProxies bool           `json:"searchingForSessionCreatorProxies"`
 }
